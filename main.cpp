a72a13d8df92d54a7367ca891fd59ef6138efa6e
Mandros7 <hectorrogue.9@gmail.com>
2016-01-06 20:32:56 +0100

    Updated system functions. Absolute timestamps implemented in the loop

--- main.cpp
+++ main.cpp
@@ -8,6 +8,9 @@
 #include "vcomptabla.h"
 #include <iostream>
 #include <unistd.h>
+#include <time.h>
+#include <stdlib.h>
+#include <pthread.h>
 
 using namespace std;
 
@@ -27,18 +30,36 @@ VCompTabla uk2TablaSalidas(100);
 
 struct datosSistema{
     VCompTabla* tablaSalida;
-    int tEspera;
+    struct timespec periodo;
     FDT * fdt;
 };
+
+void add_timespec (struct timespec *suma,
+                   const struct timespec *sumando1,
+                   const struct timespec *sumando2 )
+{
+    suma->tv_sec = sumando1->tv_sec  + sumando2->tv_sec;  // calcula segundos
+    suma->tv_nsec = sumando1->tv_nsec + sumando2->tv_nsec; // calcula nsegundos
+    if (suma->tv_nsec >= 1E9)  // ver si tenemos un segundo completo
+    {
+        suma->tv_sec ++;		// añadir 1 a la cuenta de segundos
+        suma->tv_nsec -= 1E9;	// calcular los ns que quedan
+    }
+}
 
 void * sistema (void * param){
     datosSistema* datos = (datosSistema *)param;
     FDT * p = (FDT*) datos->fdt;
+    struct timespec periodo,siguiente_activacion;
+    periodo = datos->periodo;
+    clock_gettime(CLOCK_REALTIME, &siguiente_activacion);
     while(1){
         double entrada = p->read();
         double salida = p->simular(entrada);
         datos->tablaSalida->add(salida);
-        usleep(datos->tEspera);
+
+        add_timespec(&siguiente_activacion, &siguiente_activacion, &periodo);
+        clock_nanosleep(CLOCK_REALTIME, TIMER_ABSTIME,&siguiente_activacion, NULL);
     }
 
     return 0;
@@ -67,23 +88,19 @@ int main(int argc, char *argv[])
                        -0.82};
 
     double num2[] = {0,
-                    0.002059,
-                    0.001686
+                    1.999E-5
                    };
 
     double den2[] = {1,
-                    -1.511,
-                    0.5488};
+                    -0.999};
 
-    double pid_num2[] = {414.1,
-                        -647.8,
-                        250.1,
+    double pid_num2[] = {0.2*2445.4,
+                        -1.998*2445.4,
                         0};
 
-    double pid_den2[] = {1,
-                        0.82,
-                        -1,
-                       -0.82};
+    double pid_den2[] = {0,
+                        1,
+                       -0.9998};
 
 
 
@@ -93,22 +110,26 @@ int main(int argc, char *argv[])
 
     Planta *p1 = new Planta(&kp0,&uk1,&yk1,num1,den1,3);
     Regulador *r1 = new Regulador(&kp1,&ref1,&uk1,pid_num1,pid_den1,4,&c,&s1,1);
-    Planta *p2 = new Planta(&kp0,&uk2,&yk2,num2,den2,3);
-    Regulador *r2 = new Regulador(&kp2,&ref2,&uk2,pid_num2,pid_den2,4,&c,&s2,2);
+    Planta *p2 = new Planta(&kp0,&uk2,&yk2,num2,den2,2);
+    Regulador *r2 = new Regulador(&kp2,&ref2,&uk2,pid_num2,pid_den2,3,&c,&s2,2);
 
     struct datosSistema planta1,regulador1,planta2,regulador2;
     planta1.fdt = p1;
     planta1.tablaSalida = &yk1TablaSalidas;
-    planta1.tEspera = 500000;
+    planta1.periodo.tv_sec=0;
+    planta1.periodo.tv_nsec=300000000;
     regulador1.fdt = r1;
     regulador1.tablaSalida = &uk1TablaSalidas;
-    regulador1.tEspera = 500000;
+    regulador1.periodo.tv_sec=0;
+    regulador1.periodo.tv_nsec=300000000;
     planta2.fdt = p2;
     planta2.tablaSalida = &yk2TablaSalidas;
-    planta2.tEspera = 500000;
+    planta2.periodo.tv_sec=0;
+    planta2.periodo.tv_nsec=300000000;
     regulador2.fdt = r2;
     regulador2.tablaSalida = &uk2TablaSalidas;
-    regulador2.tEspera = 500000;
+    regulador2.periodo.tv_sec=0;
+    regulador2.periodo.tv_nsec=300000000;
 
     pthread_t pla1, reg1, pla2, reg2;		/* threads que se van a crear */
 
